5f7f194b2ed1e2fcbbe63a94b0f45a456028f341
Thomas T <huulbaek@gmail.com>
2022-06-14 16:00:26 +0200

    Black header

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="bg-primary">
+  <header class="bg-black">
     <nav class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
       <div class="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none" style="height: 70px;">
         <div class="flex items-center overflow-visible">
@@ -9,14 +9,14 @@
               <Logo />
             </ClientOnly>
           </NuxtLink>
-          <div class="hidden space-x-8 lg:block">
-            <NuxtLink v-for="link in navigation" :key="link.name" :to="link.href" class="text-base font-bold uppercase text-black hover:text-indigo-50">
+          <div class="hidden space-x-8 lg:block lg:ml-5">
+            <NuxtLink v-for="link in navigation" :key="link.name" :to="link.href" class="text-base font-bold uppercase text-white hover:text-indigo-50">
               {{ link.name }}
             </NuxtLink>
           </div>
         </div>
         <div class="grid justify-items-end lg:hidden">
-          <NuxtLink v-for="link in navigation" :key="link.name" :to="link.href" class="text-base font-bold uppercase text-black hover:text-indigo-50">
+          <NuxtLink v-for="link in navigation" :key="link.name" :to="link.href" class="text-base font-bold uppercase text-white hover:text-indigo-50">
           {{ link.name }}
           </NuxtLink>
         </div>
