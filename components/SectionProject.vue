12be587d2956d6ad29e3e307692bde563270caab
Thomas T <huulbaek@gmail.com>
2022-06-24 10:09:04 +0200

    NuxtLink

--- components/SectionProject.vue
+++ components/SectionProject.vue
@@ -73,7 +73,9 @@
             {{ cta.text }}
           </p>
           <div class="inline-flex rounded-md shadow">
-            <a href="#" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">{{ cta.buttonText }}</a>
+            <NuxtLink :to="cta.link" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
+              {{ cta.buttonText }}
+            </NuxtLink>
           </div>
         </div>
       </div>
