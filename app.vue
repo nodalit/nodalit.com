fcf8a76c531c2faa59ade870f2e85b30894ffb7c
Thomas T <huulbaek@gmail.com>
2022-06-29 13:34:44 +0200

    Favicon

--- app.vue
+++ app.vue
@@ -4,3 +4,12 @@
     <NuxtPage />
   </div>
 </template>
+
+<script lang="ts" setup>
+useHead({
+  title: 'Meetr – web apps til mennesker',
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
+  ],
+})
+</script>
