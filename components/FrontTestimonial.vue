4e67233365ecb091f51d27444351440acd338ce1
Thomas T <huulbaek@gmail.com>
2022-06-30 20:23:46 +0200

    css

--- components/FrontTestimonial.vue
+++ components/FrontTestimonial.vue
@@ -5,9 +5,6 @@
         <div class="relative lg:-my-8 hidden lg:block">
           <div aria-hidden="true" class="absolute inset-x-0 top-0 h-1/2 bg-white lg:hidden" />
           <div class="rounded-2xl relative">
-            <!-- <div class="aspect-w-10 aspect-h-6 rounded-xl shadow-xl overflow-hidden sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
-              <img class="object-cover lg:h-full lg:w-full" src="/assets/link_rose_1.png" alt="LINK Kompass" />
-            </div> -->
             <div id="lottieContainerMobile" ref="lottieRef" class="absolute" style="width: 1000px; left: -300px; top: -50px"></div>
           </div>
         </div>
@@ -18,9 +15,21 @@
                 <img src="/assets/link_arkitektur_logo.svg" class="h-24 lg:h-12" />
                 <p class="mt-6 text-2xl font-medium text-black">{{ testimonial.quote }}</p>
               </div>
-              <footer class="mt-6">
-                <p class="text-base font-medium text-black">{{ testimonial.name }}</p>
-                <p class="text-base font-medium text-black">{{ testimonial.title }} @ {{ testimonial.company }}</p>
+              <footer class="mt-6 md:flex">
+                <div class="md:flex md:items-center md:justify-center">
+                  <div class="md:flex-shrink-0">
+                    <img class="mx-auto h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                  </div>
+                  <div class="mt-3 text-center md:mt-0 md:ml-4 md:flex md:items-center">
+                    <div class="text-base font-medium text-gray-900">{{ testimonial.name }}</div>
+
+                    <svg class="hidden md:block mx-1 h-5 w-5 text-logo3" fill="currentColor" viewBox="0 0 20 20">
+                      <path d="M11 0h3L9 20H6l5-20z" />
+                    </svg>
+
+                    <div class="text-base font-medium text-gray-500">{{ testimonial.title }}, {{ testimonial.company }}</div>
+                  </div>
+                </div>
               </footer>
             </blockquote>
           </div>
