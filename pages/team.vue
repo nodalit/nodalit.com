a90b5e3315ffd8dc27e984bf16c5a051c2ebadbf
Thomas T <huulbaek@gmail.com>
2022-07-25 14:59:47 +0200

    Play with mix blend bg color

--- pages/team.vue
+++ pages/team.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-white">
-    <div class="mx-auto py-10 px-4 max-w-7xl sm:px-6 lg:px-8">
+  <div>
+    <div class="mx-auto py-10 px-4 max-w-7xl sm:px-6 lg:px-8 mt-10">
       <div class="space-y-12 lg:grid lg:grid-cols-3 lg:gap-8 lg:space-y-0">
         <div class="space-y-5 sm:space-y-4">
           <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">
