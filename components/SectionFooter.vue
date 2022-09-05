87fa8b59ae924d07ca608bf134da359705f96aa3
mads <madsringblom@gmail.com>
2022-09-05 19:27:57 +0200

    footer border top

--- components/SectionFooter.vue
+++ components/SectionFooter.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="px-4 py-4 bg-gray-900 lg:pb-14 lg:overflow-hidden">
-    <div class="mx-auto max-w-7xl lg:px-8 pb-96 flex flex-col md:flex-row items-center md:justify-center text-white text-xl">
+  <div class="px-4 bg-gray-900 lg:pb-6 lg:overflow-hidden">
+    <div class="border-t-2 border-white mx-auto max-w-7xl py-4 px-8 flex flex-col md:flex-row items-center md:justify-center text-white text-xl">
       <span>CVR: 42252301</span>
       <span class="hidden md:inline-block mx-2">|</span>
       <span class="text-center">Adresse: Skovbogårds Allé 15, 2500 Valby</span>
