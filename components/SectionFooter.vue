11888657dc03f56b20caf0d48aff74917cc542eb
mads <madsringblom@gmail.com>
2022-09-05 19:01:28 +0200

    footer with cvr and adress

--- components/SectionFooter.vue
+++ components/SectionFooter.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="py-4 bg-gray-900 lg:pb-14 lg:overflow-hidden">
-    <div class="mx-auto max-w-7xl lg:px-8 pb-96">
+  <div class="px-4 py-4 bg-gray-900 lg:pb-14 lg:overflow-hidden">
+    <div class="mx-auto max-w-7xl lg:px-8 pb-96 flex flex-col md:flex-row items-center md:justify-center text-white text-xl">
+      <span>CVR: 42252301</span>
+      <span class="hidden md:inline-block mx-2">|</span>
+      <span class="text-center">Adresse: Skovbogårds Allé 15, 2500 Valby</span>
     </div>
   </div>
 </template>
