374efbfbcccedecc38eae753628f87e1c96327e3
mads <madsringblom@gmail.com>
2022-09-06 10:12:50 +0200

    footer styling

--- components/SectionFooter.vue
+++ components/SectionFooter.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="px-4 bg-gray-900 lg:pb-6 lg:overflow-hidden">
-    <div class="border-t-2 border-white mx-auto max-w-7xl py-4 px-8 flex flex-col md:flex-row items-center md:justify-center text-white text-xl">
+  <BaseSection id="kontakt" class="overflow-hidden" :bg-color="'gray-900'">
+    <div class="border-t-2 md:border-t-0 py-6 border-white flex flex-col md:flex-row items-center md:justify-center text-white text-xl">
       <span>CVR: 42252301</span>
       <span class="hidden md:inline-block mx-2">|</span>
       <span class="text-center">Adresse: Skovbogårds Allé 15, 2500 Valby</span>
     </div>
-  </div>
+  </BaseSection>
 </template>
 
 <script setup lang="ts">
